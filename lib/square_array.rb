92d9f8069768d1c86396506c6da17ae1fef78b19
Emma Fewer <emmafewer20@gmail.com>
2020-10-16 23:20:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(numbers)
   numbers = [1,2,3]
-  new_numbers = [9,10,24,25]
+  new_numbers = [9,10,16,25]
   new_array = []
   b=2
   numbers.length.times do |i|
     new_array.push(numbers[i]**b)
   end
-  new_array
+  new_array(new_numbers)
 end
